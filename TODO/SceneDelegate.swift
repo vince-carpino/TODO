d1fac09aedfa99a11b35aa83cb328b54d6a0b0c1
Vince Carpino <vcarpino33@gmail.com>
2020-05-15 15:47:06 -0700

    Adds CoreData for AppDelegate and SceneDelegate

--- TODO/SceneDelegate.swift
+++ TODO/SceneDelegate.swift
@@ -19,11 +19,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ItemListView(items: [
-            Item(name: "item 1", id: 0),
-            Item(name: "item 2", id: 1)
-        ])
+        // Get the managed object context from the shared persistent container.
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
+        // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
+        let contentView = ItemListView().environment(\.managedObjectContext, context)
+
+//        // Create the SwiftUI view that provides the window contents.
+//        let contentView = ItemListView()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
@@ -60,6 +64,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
