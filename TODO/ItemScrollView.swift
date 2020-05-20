75b6a6aa419acf0ddcf89429b2d719612def8e3c
Vince Carpino <vcarpino33@gmail.com>
2020-05-19 23:58:40 -0700

    Filters ItemScrollView to only show non-deleted items and sorts by creationTime

--- TODO/ItemScrollView.swift
+++ TODO/ItemScrollView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 struct ItemScrollView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Item.entity(), sortDescriptors: []) var items: FetchedResults<Item>
+    @FetchRequest(entity: Item.entity(), sortDescriptors: [ NSSortDescriptor(key: "creationTime", ascending: true) ]) var items: FetchedResults<Item>
 
     var body: some View {
         ZStack {
@@ -20,7 +20,7 @@ struct ItemScrollView: View {
 
             ScrollView(showsIndicators: false) {
                 VStack(spacing: 40) {
-                    ForEach(items, id: \.id) { item in
+                    ForEach(items.filter { !$0.hasBeenDeleted }, id: \.id) { item in
                         ItemPreview(item: item)
                     }
                 }
@@ -48,14 +48,23 @@ struct ItemScrollView_Previews: PreviewProvider {
         let item1 = Item(context: moc)
         item1.id = UUID()
         item1.name = "Wash car"
+        item1.creationTime = Date()
 
         let item2 = Item(context: moc)
         item2.id = UUID()
-        item2.name = "Throw in laundry"
+        item2.name = "Should not be seen"
+        item2.creationTime = Date()
+        item2.hasBeenDeleted = true
 
         let item3 = Item(context: moc)
         item3.id = UUID()
-        item3.name = String(repeating: "Just a really long name ", count: 5)
+        item3.name = "Throw in laundry"
+        item3.creationTime = Date()
+
+        let item4 = Item(context: moc)
+        item4.id = UUID()
+        item4.name = String(repeating: "Just a really long name ", count: 5)
+        item4.creationTime = Date()
 
         return ItemScrollView().environment(\.managedObjectContext, moc)
     }
