0c5b0477da31758299d7cced368b12f9c6423f82
Vince Carpino <vcarpino33@gmail.com>
2020-05-19 21:51:39 -0700

    Adds dark background and AddItemButton to ItemScrollView

--- TODO/ItemScrollView.swift
+++ TODO/ItemScrollView.swift
@@ -14,12 +14,27 @@ struct ItemScrollView: View {
     @FetchRequest(entity: Item.entity(), sortDescriptors: []) var items: FetchedResults<Item>
 
     var body: some View {
-        VStack {
-            ScrollView(.vertical, showsIndicators: true) {
-                VStack(spacing: 20) {
+        ZStack {
+            Color(red: 44/255, green: 62/255, blue: 80/255)
+                .edgesIgnoringSafeArea(.all)
+
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 40) {
                     ForEach(items, id: \.id) { item in
                         ItemPreview(item: item)
                     }
+                }
+            }
+            .padding()
+
+            VStack {
+                Spacer()
+
+                HStack {
+                    Spacer()
+
+                    AddItemButton()
+                    .padding()
                 }
             }
         }
@@ -29,12 +44,19 @@ struct ItemScrollView: View {
 struct ItemScrollView_Previews: PreviewProvider {
     static var previews: some View {
         let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
         let item1 = Item(context: moc)
         item1.id = UUID()
         item1.name = "Wash car"
+
         let item2 = Item(context: moc)
         item2.id = UUID()
         item2.name = "Throw in laundry"
+
+        let item3 = Item(context: moc)
+        item3.id = UUID()
+        item3.name = String(repeating: "Just a really long name ", count: 5)
+
         return ItemScrollView().environment(\.managedObjectContext, moc)
     }
 }
