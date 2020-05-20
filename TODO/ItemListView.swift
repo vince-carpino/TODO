5e71dec6f134853d5bd1e28c4c44019aeb357189
Vince Carpino <vcarpino33@gmail.com>
2020-05-19 21:49:59 -0700

    Cleans up ItemListView

--- TODO/ItemListView.swift
+++ TODO/ItemListView.swift
@@ -10,81 +10,43 @@ import CoreData
 import SwiftUI
 
 struct ItemListView: View {
-//    @State private var items = [Item]()
-//    @State private var items: [Item]
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Item.entity(), sortDescriptors: []) var items: FetchedResults<Item>
 
-    @State private var showingAddItemView = false
-
     var body: some View {
-        VStack {
-            List {
-                ForEach(items, id: \.id) { item in
-                    Text(item.name ?? "NAME NOT FOUND")
+        ZStack {
+            NavigationView {
+                List(items, id: \.id) { item in
+                    NavigationLink(destination: ItemDetailView(item: item)) {
+                        Text(item.name ?? "NAME NOT FOUND")
+                    }
+                }
+                .navigationBarTitle(Text("Items"))
+            }
+
+            VStack {
+                Spacer()
+
+                HStack {
+                    Spacer()
+
+                    AddItemButton()
+                    .padding()
                 }
             }
-
-            Button("Add") {
-                self.showingAddItemView.toggle()
-//                let itemNames = [
-//                    "One",
-//                    "Two",
-//                    "Three",
-//                    "Four",
-//                    "Five"
-//                ]
-//
-//                let item = Item(context: self.moc)
-//                item.id = UUID()
-//                item.name = "\(itemNames.randomElement()!) \(itemNames.randomElement()!)"
-//
-//                do {
-//                    try self.moc.save()
-//                } catch {
-//                    print("THERE WAS AN ERROR SAVING ITEM")
-//                }
-            }
-            .sheet(isPresented: $showingAddItemView) {
-                AddItemView().environment(\.managedObjectContext, self.moc)
-            }
         }
-
-//        ZStack {
-//            NavigationView {
-//                List(items, id: \.id) { item in
-//                    NavigationLink(destination: ItemDetailView(item: item)) {
-//                        Text(item.name ?? "NAME NOT FOUND")
-//                    }
-//                }
-//                .navigationBarTitle(Text("Items"))
-//
-//                VStack {
-//                    Spacer()
-//
-//                    HStack {
-//                    Button(action: {
-//        //                        print("\n\nItems current: \(self.items)")
-//        //                        print("\nDefaults current: \(self.getDecodedItemsList())")
-//                    }) {
-//                        Text("Refresh")
-//                    }
-//                    .padding()
-//
-//                        Spacer()
-//
-//                        AddItemButton(showAddView: $showingAddItemView)
-//                            .padding()
-//                    }
-//                }
-//            }
-//        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-//        ItemListView(itemsList: Binding.constant([Item()]))
-        ItemListView()
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let item1 = Item(context: moc)
+        item1.id = UUID()
+        item1.name = "Wash car"
+        let item2 = Item(context: moc)
+        item2.id = UUID()
+        item2.name = "Throw in laundry"
+        return ItemListView().environment(\.managedObjectContext, moc)
     }
 }
