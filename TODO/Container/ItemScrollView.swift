b946da58d04978bcae1e0eea752c6b8f044e2e02
Vince Carpino <vcarpino33@gmail.com>
2020-06-11 16:14:26 -0700

    Adds dueDate to ItemScrollView preview items

--- TODO/Container/ItemScrollView.swift
+++ TODO/Container/ItemScrollView.swift
@@ -48,27 +48,32 @@ struct ItemScrollView: View {
 struct ItemScrollView_Previews: PreviewProvider {
     static var previews: some View {
         let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let oneDayInSeconds = Double(24 * 60 * 60)
 
         let item1 = Item(context: moc)
         item1.id = UUID()
         item1.name = "Wash car"
         item1.creationTime = Date()
+        item1.dueDate = Date()
 
         let item2 = Item(context: moc)
         item2.id = UUID()
         item2.name = "Should not be seen"
         item2.creationTime = Date()
         item2.hasBeenDeleted = true
+        item2.dueDate = Date().advanced(by: 1 * oneDayInSeconds)
 
         let item3 = Item(context: moc)
         item3.id = UUID()
         item3.name = "Throw in laundry"
         item3.creationTime = Date()
+        item3.dueDate = Date().advanced(by: -2 * oneDayInSeconds)
 
         let item4 = Item(context: moc)
         item4.id = UUID()
         item4.name = String(repeating: "Just a really long name ", count: 5)
         item4.creationTime = Date()
+        item4.dueDate = Date().advanced(by: 3 * oneDayInSeconds)
 
         return ItemScrollView().environment(\.managedObjectContext, moc)
     }
