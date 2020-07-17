850afd6bf3db60dbe7b194d48e2cd213ade16240
Vince Carpino <vcarpino33@gmail.com>
2020-07-16 17:09:44 -0700

    Adds LongPressMenuOptions buttons to ItemPreview

--- TODO/ItemPreview.swift
+++ TODO/ItemPreview.swift
@@ -15,36 +15,53 @@ struct ItemPreview: View {
     @FetchRequest(entity: Item.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Item.isCurrentItem, ascending: true)], predicate: NSPredicate(format: "isCurrentItem == true")) var currentItem: FetchedResults<Item>
 
     @State private var isPresentingDetail = false
+    @State private var isPresentingMenuOptions = false
 
     let item: Item
 
     var body: some View {
-        Button(action: {}) {
-            Text(item.name ?? "Unknown name")
-                .font(.system(size: 24, weight: .semibold, design: .rounded))
-                .bold()
-                .lineLimit(3)
-                .truncationMode(.tail)
-                .padding(30)
-                .foregroundColor(.clouds)
-                .background(getBackgroundColor(item: item))
-                .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .strokeBorder(Color.clouds, lineWidth: 5)
-                )
-                .onTapGesture {
-                    self.isPresentingDetail = true
-                }
-                .onLongPressGesture {
-                    self.markPreviousCurrentItemAsNotCurrent()
-                    self.item.isCompleted = false
-                    self.item.isCurrentItem = true
-                    self.saveContext()
-                }
-        }
-        .sheet(isPresented: $isPresentingDetail) {
-            ItemDetailView(item: self.item).environment(\.managedObjectContext, self.moc)
+        VStack(spacing: 40) {
+            if isPresentingMenuOptions {
+                LongPressMenuButton(isPresented: self.$isPresentingMenuOptions, iconName: "bolt.fill", label: "mark current", accentColor: .currentItemColor)
+            }
+
+            Button(action: {}) {
+                Text(item.name ?? "Unknown name")
+                    .font(.system(size: 24, weight: .semibold, design: .rounded))
+                    .bold()
+                    .lineLimit(3)
+                    .truncationMode(.tail)
+                    .padding(30)
+                    .foregroundColor(.clouds)
+                    .background(getBackgroundColor(item: item))
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .strokeBorder(Color.clouds, lineWidth: 5)
+                    )
+                    .onTapGesture(count: 2) {
+                        print("DOUBLE TAPPED")
+                    }
+                    .onTapGesture {
+                        self.isPresentingDetail = true
+                    }
+                    .onLongPressGesture {
+                        withAnimation {
+                            self.isPresentingMenuOptions = true
+                        }
+//                        self.markPreviousCurrentItemAsNotCurrent()
+//                        self.item.isCompleted = false
+//                        self.item.isCurrentItem = true
+//                        self.saveContext()
+                    }
+            }
+            .sheet(isPresented: $isPresentingDetail) {
+                ItemDetailView(item: self.item).environment(\.managedObjectContext, self.moc)
+            }
+
+            if isPresentingMenuOptions {
+                LongPressMenuButton(isPresented: self.$isPresentingMenuOptions, iconName: "checkmark.square.fill", label: "mark complete", accentColor: .completedItemColor)
+            }
         }
     }
 
@@ -89,5 +106,40 @@ struct ItemPreview_Previews: PreviewProvider {
 
             ItemPreview(item: item)
         }
+    }
+}
+
+struct LongPressMenuButton: View {
+    @Binding var isPresented: Bool
+
+    var iconName: String
+    var label: String
+    var accentColor: Color
+
+    var body: some View {
+        Button(action: {
+            withAnimation {
+                self.isPresented = false
+            }
+        }) {
+            VStack {
+                Image(systemName: iconName)
+                    .frame(width: 60, height: 60)
+                    .imageScale(.large)
+                    .foregroundColor(.clouds)
+                    .background(accentColor)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.clouds, lineWidth: 3)
+                    )
+                Text(label)
+                    .font(.system(size: 16, weight: .semibold, design: .rounded))
+                    .padding(5)
+                    .background(Color.clouds)
+                    .cornerRadius(5)
+                    .foregroundColor(accentColor)
+            }
+        }
+        .transition(.scale)
     }
 }
