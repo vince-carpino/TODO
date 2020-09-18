9368c2ed3fa657f10ae4d35d0299b13c10628100
Vince Carpino <vcarpino33@gmail.com>
2020-09-18 15:55:31 -0700

    Adds TimeBlockHelperTests and removes TODO tests

---
+++ TaskMasteryUnitTests/TimeBlockHelperTests.swift
@@ -0,0 +1,32 @@
+//
+//  TimeBlockHelperTests.swift
+//  TimeBlockHelperTests
+//
+//  Created by Vince Carpino on 9/18/20.
+//  Copyright © 2020 Vince Carpino. All rights reserved.
+//
+
+import XCTest
+@testable import Task_Mastery
+
+class TimeBlockHelperTests: XCTestCase {
+
+    func testGetFinalTimeBlocks() {
+        let originalTimeBlocks: [TimeBlock] = [
+            TimeBlock(name: "first item", color: .red, startTime: 8, endTime: 9),
+            TimeBlock(name: "second item", color: .orange, startTime: 9, endTime: 10),
+            TimeBlock(name: "third item", color: .yellow, startTime: 11, endTime: 12)
+        ]
+
+        let expectedFinalTimeBlocks: [TimeBlock] = [
+            TimeBlock(name: "first item", color: .red, startTime: 8, endTime: 9),
+            TimeBlock(name: "second item", color: .orange, startTime: 9, endTime: 10),
+            TimeBlockSpacer(startTime: 10, endTime: 11),
+            TimeBlock(name: "third item", color: .yellow, startTime: 11, endTime: 12)
+        ]
+
+        let actualFinalTimeBlocks: [TimeBlock] = TimeBlockHelper.getFinalTimeBlocks(originalTimeBlocks: originalTimeBlocks)
+
+        XCTAssertEqual(actualFinalTimeBlocks, expectedFinalTimeBlocks)
+    }
+}
