a779ac2b222fcc408305626a94621e95241b43ad
Vince Carpino <vcarpino33@gmail.com>
2021-01-29 15:48:57 -0800

    Adds FormattedText ViewModifier and formatted View extension

--- TODO/TimelineView.swift
+++ TODO/TimelineView.swift
@@ -243,6 +243,23 @@ struct TimelineSeparator: View {
     }
 }
 
+struct FormattedText: ViewModifier {
+    let fontSize: CGFloat
+    let color: Color
+
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(color)
+            .font(.system(size: fontSize, weight: .semibold, design: .rounded))
+    }
+}
+
+extension View {
+    func formatted(fontSize: CGFloat, foregroundColor: Color = .clouds) -> some View {
+        modifier(FormattedText(fontSize: fontSize, color: foregroundColor))
+    }
+}
+
 struct BackgroundView: View {
     var body: some View {
         Color.backgroundColor
